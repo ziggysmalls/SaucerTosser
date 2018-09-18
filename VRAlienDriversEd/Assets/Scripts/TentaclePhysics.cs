b3cb9a47b9fdbfee169171dc8e55920cffc029cf
offlebaggins <nickburt06@gmail.com>
2018-09-18 12:04:57 -0500

    Tentacle/Cockpit Interaction Prototype

---
+++ VRAlienDriversEd/Assets/Scripts/TentaclePhysics.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentaclePhysics : MonoBehaviour {
+
+	public float speed = 5;
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+
+		float step = speed * Time.deltaTime;
+		gameObject.transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
