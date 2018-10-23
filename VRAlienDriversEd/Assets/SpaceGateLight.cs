afae80696082ca5ac4473e4c21dae683b3572251
offlebaggins <nicholas.burt@purchase.edu>
2018-10-23 14:31:26 -0500

    Made everything not broken again

---
+++ VRAlienDriversEd/Assets/SpaceGateLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceGateLight : MonoBehaviour {
+
+    public SpaceGate spaceGate;
+    MeshRenderer rend;
+    public Material triggerMat;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spaceGate.isTriggered)
+        {
+            rend.material = triggerMat;
+        }
+	}
+}
