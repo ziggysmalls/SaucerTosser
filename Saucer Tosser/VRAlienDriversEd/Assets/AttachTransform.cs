cda0f6749e76d57c66eb58a1dec59f527890348a
offlebaggins <nickburt06@gmail.com>
2018-10-25 17:08:24 -0500

    WIP Fix for ship physics (non functional but proof of concept is there)

---
+++ Saucer Tosser/VRAlienDriversEd/Assets/AttachTransform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachTransform : MonoBehaviour {
+
+    public Transform targetTransform;
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = targetTransform.position;
+        transform.rotation = targetTransform.rotation;
+    }
+}
