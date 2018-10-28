01ac1f5ac9293123bc563f9a7417df299cf59d40
offlebaggins <nickburt06@gmail.com>
2018-10-28 11:37:41 -0500

    ROUGH raido first pass

--- Saucer Tosser/VRAlienDriversEd/Assets/Radio.cs
+++ Saucer Tosser/VRAlienDriversEd/Assets/Radio.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK.Controllables.PhysicsBased;
 
 public class Radio : MonoBehaviour {
 
     public AudioClip[] songs;
-    public GameObject powerButton;
-    public GameObject volumeUpButton;
-    public GameObject volumeDownButton;
-    public GameObject nextSongButton;
-    public GameObject lastSongButton;
+    public VRTK_PhysicsPusher powerButton;
+    public VRTK_PhysicsPusher volumeUpButton;
+    public VRTK_PhysicsPusher volumeDownButton;
+    public VRTK_PhysicsPusher nextSongButton;
+    public VRTK_PhysicsPusher lastSongButton;
+    bool nextSongPressed = false;
+    bool lastSongPressed = false;
+    bool volumeUpPressed = false;
+    bool volumeDownPressed = false;
+    bool powerButtonPressed = false;
+    bool isPowered = false;
+
+
 
     int currentSong = 0;
     AudioSource audioSource;
@@ -18,22 +27,68 @@ public class Radio : MonoBehaviour {
     void Start () {
         audioSource = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject == nextSongButton)
-            currentSong++;
-        if (collision.gameObject == lastSongButton)
-            currentSong--;
+    // Update is called once per frame
+    void Update() {
+        if (nextSongButton.GetNormalizedValue() == 1)
+        {
+            if (!nextSongPressed)
+            {
+                nextSongPressed = true;
+                currentSong++;
+                if (currentSong < 0) currentSong = songs.Length-1;
+                audioSource.clip = songs[currentSong];
+                audioSource.Play();
+            }
+        }
+        else nextSongPressed = false;
 
-        if (currentSong > songs.Length) currentSong = 0;
-        audioSource.clip = songs[currentSong];
-        audioSource.Play();
+        if (lastSongButton.GetNormalizedValue() == 1)
+        {
+            if (!lastSongPressed)
+            {
+                lastSongPressed = true;
+                currentSong++;
+                if (currentSong > songs.Length) currentSong = 0;
+                audioSource.clip = songs[currentSong];
+                audioSource.Play();
+            }
+        }
+        else lastSongPressed = false;
+
+        if (volumeUpButton.GetNormalizedValue() == 1)
+        {
+            if (!volumeUpPressed)
+            {
+                volumeUpPressed = true;
+                audioSource.volume -= .2f;
+                if (audioSource.volume < 0f) audioSource.volume = 0;
+            }
+        }
+        else volumeUpPressed = false;
+
+        if (volumeUpButton.GetNormalizedValue() == 1)
+        {
+            if (!volumeUpPressed)
+            {
+                volumeUpPressed = true;
+                audioSource.volume += .2f;
+                if (audioSource.volume > 1f) audioSource.volume = 1;
+            }
+        }
+        else volumeDownPressed = false;
+
+
+        if (powerButton.GetNormalizedValue() == 1)
+        {
+            if (!powerButtonPressed)
+            {
+                powerButtonPressed = true;
+                isPowered = !isPowered;
+                audioSource.enabled = isPowered;
+            }
+        }
+        else powerButtonPressed = false;
     }
 
 }
---
+++ Saucer Tosser/VRAlienDriversEd/Assets/Structure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+
+    GameObject[] children;
+    // Use this for initialization
+    void Start()
+    {
+        children = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i).gameObject;
+        }
+        foreach (GameObject child in children)
+        {
+            child.isStatic = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (GameObject child in children)
+        {
+            child.isStatic = false;
+        }
+    }
+}
