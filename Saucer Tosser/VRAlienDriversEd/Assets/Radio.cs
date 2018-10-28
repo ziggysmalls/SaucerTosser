e365d0915c5f23e659bd80e609788e314532b122
offlebaggins <nickburt06@gmail.com>
2018-10-27 22:27:08 -0500

    Made game more better

--- Saucer Tosser/VRAlienDriversEd/Assets/Radio.cs
+++ Saucer Tosser/VRAlienDriversEd/Assets/Radio.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class Radio : MonoBehaviour {
 
     public AudioClip[] songs;
+    public GameObject powerButton;
+    public GameObject volumeUpButton;
+    public GameObject volumeDownButton;
+    public GameObject nextSongButton;
+    public GameObject lastSongButton;
 
-	// Use this for initialization
-	void Start () {
-		
+    int currentSong = 0;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == nextSongButton)
+            currentSong++;
+        if (collision.gameObject == lastSongButton)
+            currentSong--;
+
+        if (currentSong > songs.Length) currentSong = 0;
+        audioSource.clip = songs[currentSong];
+        audioSource.Play();
+    }
 
 }
