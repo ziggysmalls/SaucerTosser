cde43e907ee6aeeb656e3334bf2bf5e84fc89aae
offlebaggins <nickburt06@gmail.com>
2018-10-28 21:13:16 -0500

    Tractor Beam Prototype

---
+++ Saucer Tosser/VRAlienDriversEd/Assets/Pulley.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulley : MonoBehaviour {
+
+    public float triggerDistance = 3f;
+    public GameObject tractorBeam;
+
+	// Update is called once per frame
+	void Update () {
+        if (transform.localPosition.y > triggerDistance)
+        {
+            ActivateTractorBeam();
+            Debug.Log("ACTIVE BEAM");
+        }else{
+            DeactivateTractorBeam();
+            Debug.Log("INACTIVE BEAM");
+        }
+        Debug.Log(transform.localPosition.y);
+        Debug.Log(triggerDistance);
+
+    }
+
+    public void ActivateTractorBeam()
+    {
+        tractorBeam.SetActive(true);
+    }
+
+    public void DeactivateTractorBeam()
+    {
+        tractorBeam.SetActive(false);
+    }
+}
---
+++ Saucer Tosser/VRAlienDriversEd/Assets/TractorBeam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TractorBeam : MonoBehaviour {
+
+    public float speed = 1;
+    public GameObject UFO;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+
+        if (col.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+            //rb.AddForce(new Vector3(0, speed, 0));
+            col.transform.position = Vector3.MoveTowards(col.transform.position, UFO.transform.position, speed);
+            rb.useGravity = false;
+            Debug.Log(col);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+    }
+}
